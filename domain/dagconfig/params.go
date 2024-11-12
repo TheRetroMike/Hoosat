da8b34f7d88f2b2229736482e7a116cae8433554
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2024-11-12 09:02:43 +0200

    Add testnet for block version 3 to 15 DAA Score

--- domain/dagconfig/params.go
+++ domain/dagconfig/params.go
@@ -341,7 +341,7 @@ var TestnetParams = Params{
 
 	MaxBlockLevel: 250,
 	MergeDepth:    defaultMergeDepth,
-	POWScores:     []uint64{5},
+	POWScores:     []uint64{5, 15},
 }
 
 // SimnetParams defines the network parameters for the simulation test Hoosat
