62105fb42ea1466cb387f7402546e0cf5808af6a
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2025-01-29 18:14:57 +0200

    Don't send selected virtual parent if it's block version requires PoW hash.

--- app/protocol/flows/v5/blockrelay/send_virtual_selected_parent_inv.go
+++ app/protocol/flows/v5/blockrelay/send_virtual_selected_parent_inv.go
@@ -3,9 +3,12 @@ package blockrelay
 import (
 	"github.com/Hoosat-Oy/HTND/app/appmessage"
 	peerpkg "github.com/Hoosat-Oy/HTND/app/protocol/peer"
+	"github.com/Hoosat-Oy/HTND/app/protocol/protocolerrors"
 	"github.com/Hoosat-Oy/HTND/domain"
+	"github.com/Hoosat-Oy/HTND/domain/consensus/utils/constants"
 	"github.com/Hoosat-Oy/HTND/infrastructure/config"
 	"github.com/Hoosat-Oy/HTND/infrastructure/network/netadapter/router"
+	"github.com/pkg/errors"
 )
 
 // SendVirtualSelectedParentInvContext is the interface for the context needed for the SendVirtualSelectedParentInv flow.
@@ -22,14 +25,25 @@ func SendVirtualSelectedParentInv(context SendVirtualSelectedParentInvContext,
 	if err != nil {
 		return err
 	}
-
 	if virtualSelectedParent.Equal(context.Config().NetParams().GenesisHash) {
-		log.Debugf("Skipping sending the virtual selected parent hash to peer %s because it's the genesis", peer)
+		log.Debugf("Skipping sending the virtual selected parent hash to peer %s because it's the genesis.", peer)
 		return nil
+	}
+	block, found, err := context.Domain().Consensus().GetBlock(virtualSelectedParent)
+	if block.Header.Version() >= constants.PoWIntegrityMinVersion {
+		if err != nil {
+			return errors.Wrapf(err, "unable to fetch requested virtual selected parent block %s", virtualSelectedParent)
+		}
+		if !found {
+			return protocolerrors.Errorf(false, "virtual selected parent block %s not found", virtualSelectedParent)
+		}
+		if block.PoWHash == "" {
+			log.Debugf("Skipping sending the virtual selected parent hash to peer %s because it's PoW hash is empty.", peer)
+			return nil
+		}
 	}
 
 	log.Debugf("Sending virtual selected parent hash %s to peer %s", virtualSelectedParent, peer)
-
 	virtualSelectedParentInv := appmessage.NewMsgInvBlock(virtualSelectedParent)
 	return outgoingRoute.Enqueue(virtualSelectedParentInv)
 }
