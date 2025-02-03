b053dcce93140e6a2e2c75dc95b1ddfcdcb3b3ca
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2025-02-03 10:59:04 +0200

    Add the hard fork daa score to powscores.

--- domain/dagconfig/params.go
+++ domain/dagconfig/params.go
@@ -281,7 +281,7 @@ var MainnetParams = Params{
 	// This means that any block that has a level lower or equal to genesis will be level 0.
 	MaxBlockLevel: 225,
 	MergeDepth:    defaultMergeDepth,
-	POWScores:     []uint64{17500000, 21821800},
+	POWScores:     []uint64{17500000, 21821800, 29335426},
 }
 
 // TestnetParams defines the network parameters for the test Hoosat network.
